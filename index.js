a14e0cb5c37987bcd7b225d9d4faec8eed698305
affaqkhan211 <affaqkhan211@gmail.com>
2023-12-06 14:39:46 +0500

    cors issue resolves

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ import express from "express";
 const app = express();
 import Connection from "./db/Conn.js";
 import blogRouter from "./routes/blogRouter.js";
+import cors from "cors"
 
 const PORT = process.env.PORT || 8000;
 Connection();
 
+app.use(cors())
 app.use(express.json());
 app.use("/api/v1", blogRouter);
 
